df07d8965dc12289c54e052469db49df371337c0
Ajay Kumar <52097509+AjayKumarGitHub-dotcom@users.noreply.github.com>
2021-01-31 14:49:27 +0530

    added path package and made changes in reporting

--- src/reporting/CucumberReportExtension.ts
+++ src/reporting/CucumberReportExtension.ts
@@ -1,14 +1,18 @@
 import * as report from "cucumber-html-reporter";
 import { Options } from "cucumber-html-reporter";
+import path from 'path';
 export class CucumberReportExtension{
-    private static jsonDir = process.cwd()+"\\src\\reporting\\json";
-    private static htmlDir = process.cwd()+ "\\src\\reporting\\html";
-    private static jsonFile= CucumberReportExtension.jsonDir +"\\cucumber_report.json";
+    private static projectDir=process.cwd();
+    private static src:string =path.join(CucumberReportExtension.projectDir,'src');
+    private static reporting:string = path.join(CucumberReportExtension.src,'reporting');
+    private static jsonDir:string= path.join(CucumberReportExtension.reporting,'json');
+    private static htmlDir:string =path.join(CucumberReportExtension.reporting,'html');
+    private static jsonFile:string= path.join(CucumberReportExtension.jsonDir,'cucumber_report.json');
 
     public static cucumberReportOptions:Options ={
         theme:'bootstrap',
         jsonFile:CucumberReportExtension.jsonFile,
-        output:CucumberReportExtension.htmlDir+"\\cucumber_reporter.html",
+        output:path.join(CucumberReportExtension.htmlDir,'cucumber_reporter.html'),
         reportSuiteAsScenarios:true,
         launchReport:false,
         metadata:{
